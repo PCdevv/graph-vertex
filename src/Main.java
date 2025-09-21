4e2d799a36d6553fca0b0dfd9aaab85c3246580a
PCdevv <putericitradev@gmail.com>
2025-09-21 22:00:14 +0700

    sesuaikan contoh, jarak A-C = 150  EB = 1

--- src/Main.java
+++ src/Main.java
@@ -9,14 +9,14 @@ public class Main {
         theGraph.addVertex('D'); // 3
         theGraph.addVertex('E'); // 4
 
-        theGraph.addEdge(0, 2, 50); // AB 50
+        theGraph.addEdge(0, 2, 150); // AB 150
         theGraph.addEdge(0, 3, 80); // AD 80
         theGraph.addEdge (1, 2, 60); // BC 60
         theGraph.addEdge (1, 3, 90); // BD 90
         theGraph.addEdge(2, 4, 40); // CE 40
         theGraph.addEdge(3, 2, 20); // DC 20
         theGraph.addEdge (3, 4, 70); // DE 70
-        theGraph.addEdge(4, 2, 50); // EB 50
+        theGraph.addEdge(4, 1, 50); // EB 50
 
         theGraph.daftarTetangga ();
         theGraph.adjancyTable();
